3370f8d55978af4ebe238c3983e6189ec3686bd2
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2019-11-14 14:22:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const   tmi = require("tmi.js");
+
+const options = {
+    options: {
+      debug: true
+    },
+    connection: {
+        cluster: 'aws',
+      reconnect: true
+    },
+    identity: {
+      username: "kel_bot",
+      password: "oauth:s1ctua6ggq3bvlprav9mnns61uxqv3"
+    },
+    channels: ['ksinc'],
+  };
+
+  const client = new tmi.client(options);
+
+  client.connect();
+  //bağlanma mesajı
+client.on('connected', function (address, port) {
+   client.action('ksinc','Çalışıyorum la sakin');
+});
+
+   client.on('chat', (channel, user, message, self) => {
+    if(message === "!facebook" || message === "!fb"){
+        client.action('ksinc','=> Facebook grubumuza katılarak her ortamdan bizden haberdar olabilirsin. https://www.facebook.com/ksinctwitchgrubu/');
+    }else if(message === "!youtube" || message === "!yt"){
+        client.action('ksinc','=> Eğer yayın yokken izleyicilerin yaptığı editleri izleyip vakit geçirmek istersen beni YouTube üzerinden takip edebilirsin. https://www.youtube.com/channel/UCKs4B7UNmHkZZEqbozjbqCQ');
+    }else if(message === "!steam"){
+        client.action('ksinc','=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://steamcommunity.com/profiles/76561198053193040/');
+    }else if(message === "!donate" || message === "!bynogame"){
+        client.action('ksinc','=> Eğer sende bana bağış göndermek istersen bynogame adresime buradan ulaşabilirsin. https://www.bynogame.com/ksinctv');
+        client.action('ksinc','=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://www.durmaplay.com/tr/twitch/ksinctv');
+    }else if(message === "!instagram" || message === "!ig"){
+        client.action('ksinc','=> Yayın zamanlarını ve eğlenceli anlarımı takip etmek isterseniz Instagram üzerinden takip edebilirsiniz. https://www.instagram.com/tlhadmlu/');
+    }else if(message === "!spotify" || message === "!playlist"){
+        client.action('ksinc','=> Arkada çalan playlist işte burada. https://open.spotify.com/playlist/68yvE21YIFlUZwlkSwBl4z?si=z7ryCSZjR7SxpqAeYYvhcg');
+    }else if(message === "!discord" || message === "!dc"){
+        client.action('ksinc','=> Sunucuya gelerek sende benim o güzel sesim ile kulaklarını mutlu edebilirsin. https://discord.gg/mmXEJBN');
+    }else if(message === "!com" || message === "!komutlar"){
+        client.action('ksinc','=> !prime - !fb - !yt - !ig - !donate - !dc - !playlist - !steam');
+    }else if(message === "!prime"){
+        client.action('ksinc','=> Twitch prime ile ücretsiz abone olabilir ve bana destek verebilirsiniz https://twitch.amazon.com/tp ');
+    }
+});
