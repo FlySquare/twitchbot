2cee79dd8c4786d0cc1316e7dccf182e6b5fe8e5
Umut Can Arda <umutkonurinso@gmail.com>
2019-12-22 16:37:56 +0300

    Fixed the index.js

--- index.js
+++ index.js
@@ -12,36 +12,39 @@ const options = {
       username: "kel_bot",
       password: "oauth:s1ctua6ggq3bvlprav9mnns61uxqv3"
     },
-    channels: ['ksinc'],
+    channels: ['flysquaree'],
   };
+  // Edit channel name
+  var chnl = "flysquaree";
+  // Edit channel name
 
   const client = new tmi.client(options);
 
   client.connect();
   //bağlanma mesajı
 client.on('connected', function (address, port) {
-   client.action('ksinc','Çalışıyorum la sakin');
+   client.action(chnl,'Çalışıyorum la sakin');
 });
 
    client.on('chat', (channel, user, message, self) => {
     if(message === "!facebook" || message === "!fb"){
-        client.action('ksinc','=> Facebook grubumuza katılarak her ortamdan bizden haberdar olabilirsin. https://www.facebook.com/ksinctwitchgrubu/');
+        client.action(chnl,'=> Facebook grubumuza katılarak her ortamdan bizden haberdar olabilirsin. https://www.facebook.com/ksinctwitchgrubu/');
     }else if(message === "!youtube" || message === "!yt"){
-        client.action('ksinc','=> Eğer yayın yokken izleyicilerin yaptığı editleri izleyip vakit geçirmek istersen beni YouTube üzerinden takip edebilirsin. https://www.youtube.com/channel/UCKs4B7UNmHkZZEqbozjbqCQ');
+        client.action(chnl,'=> Eğer yayın yokken izleyicilerin yaptığı editleri izleyip vakit geçirmek istersen beni YouTube üzerinden takip edebilirsin. https://www.youtube.com/channel/UCKs4B7UNmHkZZEqbozjbqCQ');
     }else if(message === "!steam"){
-        client.action('ksinc','=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://steamcommunity.com/profiles/76561198053193040/');
+        client.action(chnl,'=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://steamcommunity.com/profiles/76561198053193040/');
     }else if(message === "!donate" || message === "!bynogame"){
-        client.action('ksinc','=> Eğer sende bana bağış göndermek istersen bynogame adresime buradan ulaşabilirsin. https://www.bynogame.com/ksinctv');
-        client.action('ksinc','=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://www.durmaplay.com/tr/twitch/ksinctv');
+        client.action(chnl,'=> Eğer sende bana bağış göndermek istersen bynogame adresime buradan ulaşabilirsin. https://www.bynogame.com/ksinctv');
+        client.action(chnl,'=> Eğer sende bana skin,oyun veya hediye göndermek istersen steam adresime buradan ulaşabilirsin. https://www.durmaplay.com/tr/twitch/ksinctv');
     }else if(message === "!instagram" || message === "!ig"){
-        client.action('ksinc','=> Yayın zamanlarını ve eğlenceli anlarımı takip etmek isterseniz Instagram üzerinden takip edebilirsiniz. https://www.instagram.com/tlhadmlu/');
+        client.action(chnl,'=> Yayın zamanlarını ve eğlenceli anlarımı takip etmek isterseniz Instagram üzerinden takip edebilirsiniz. https://www.instagram.com/tlhadmlu/');
     }else if(message === "!spotify" || message === "!playlist"){
-        client.action('ksinc','=> Arkada çalan playlist işte burada. https://open.spotify.com/playlist/68yvE21YIFlUZwlkSwBl4z?si=z7ryCSZjR7SxpqAeYYvhcg');
+        client.action(chnl,'=> Arkada çalan playlist işte burada. https://open.spotify.com/playlist/68yvE21YIFlUZwlkSwBl4z?si=z7ryCSZjR7SxpqAeYYvhcg');
     }else if(message === "!discord" || message === "!dc"){
-        client.action('ksinc','=> Sunucuya gelerek sende benim o güzel sesim ile kulaklarını mutlu edebilirsin. https://discord.gg/mmXEJBN');
+        client.action(chnl,'=> Sunucuya gelerek sende benim o güzel sesim ile kulaklarını mutlu edebilirsin. https://discord.gg/mmXEJBN');
     }else if(message === "!com" || message === "!komutlar"){
-        client.action('ksinc','=> !prime - !fb - !yt - !ig - !donate - !dc - !playlist - !steam');
+        client.action(chnl,'=> !prime - !fb - !yt - !ig - !donate - !dc - !playlist - !steam');
     }else if(message === "!prime"){
-        client.action('ksinc','=> Twitch prime ile ücretsiz abone olabilir ve bana destek verebilirsiniz https://twitch.amazon.com/tp ');
+        client.action(chnl,'=> Twitch prime ile ücretsiz abone olabilir ve bana destek verebilirsiniz https://twitch.amazon.com/tp ');
     }
 });
